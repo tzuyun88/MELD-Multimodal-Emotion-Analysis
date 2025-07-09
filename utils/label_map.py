85e563d8857e599309c7a1451d54d1d8ab828030
tzuyun88 <148322642+tzuyun88@users.noreply.github.com>
2025-07-09 09:36:52 +0800

    Create label_map.py

---
+++ utils/label_map.py
@@ -0,0 +1,11 @@
+label_map = {
+    'neutral': 0,
+    'joy': 1,
+    'surprise': 2,
+    'anger': 3,
+    'sadness': 4,
+    'disgust': 5,
+    'fear': 6
+}
+
+id2label = {v: k for k, v in label_map.items()}
